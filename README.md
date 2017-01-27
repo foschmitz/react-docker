7fc88b608e883137ad2b1823bd7d4a5f7bfef5f6
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-01-27 14:28:25 +0100

    Docker Build

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# react-docker
-React in Docker
+# React Docker Starter built with Maven
+React in Docker Container
+
+### Build with 
+mvn docker:build 
+
+### Push to registry with 
+mvn docker:push
+
+### Run with
+docker run -d -p 3000:3000 your-registry:5000/react-docker
--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,4 @@
-var Greeter = React.createClass({
+var GreeterMessage = React.createClass({
     getDefaultProps: function () {
         return {
             name: 'React',
@@ -10,18 +10,6 @@ var Greeter = React.createClass({
             name: this.props.name
         };
     },
-    onButtonClick: function (e) {
-        e.preventDefault();
-        var name = this.refs.name.value;
-        this.refs.name.value = '';
-        if (typeof name === 'string' && name.length > 0) {
-
-            this.setState({
-                name: name
-            });
-        }
-
-    },
     render: function () {
         var name = this.state.name;
         var msg = this.props.msg;
@@ -29,11 +17,43 @@ var Greeter = React.createClass({
             <div>
                 <h1>Hello {name}!</h1>
                 <p>{msg}</p>
+            </div>
+        );
+    }
+});
 
-                <form onSubmit={this.onButtonClick}>
-                    <input type="text" ref="name"/>
-                    <button>Set Name</button>
-                </form>
+var GreeterForm = React.createClass({
+    onFormSubmit: function (e) {
+        e.preventDefault();
+        var name = this.refs.name.value;
+
+        if (typeof name === 'string' && name.length > 0) {
+            this.refs.name.value = '';
+            this.props.onNewName(name);
+        }
+    },
+    render: function () {
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <input type="text" ref="name"/>
+                <button>Set Name</button>
+            </form>
+        )
+    }
+});
+
+var Greeter = React.createClass({
+
+    handleNewName: function (name) {
+        this.setState({
+            name: name
+        })
+    },
+    render: function () {
+        return (
+            <div>
+                <GreeterMessage/>
+                <GreeterForm onNewName={this.handleNewName}/>
             </div>
         )
     }
