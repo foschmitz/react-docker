1cc08615f2761a4725e05f503fd5dd6678b226f3
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-01-27 12:09:00 +0100

    This Props

---
+++ public/app.jsx
@@ -0,0 +1,16 @@
+var Greeter = React.createClass({
+    render: function () {
+        var name = this.props.name;
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+                <p>This is from the component!</p>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(
+    <Greeter name="Frank"/>,
+    document.getElementById('app')
+);
