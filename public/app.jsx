ca09c51f2aeb9c069ef2bbb04777411fc2775887
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-01-27 12:57:59 +0100

    This State

--- public/app.jsx
+++ public/app.jsx
@@ -1,16 +1,47 @@
 var Greeter = React.createClass({
+    getDefaultProps: function () {
+        return {
+            name: 'React',
+            msg: 'This is from the component!'
+        }
+    },
+    getInitialState: function () {
+        return {
+            name: this.props.name
+        };
+    },
+    onButtonClick: function (e) {
+        e.preventDefault();
+        var name = this.refs.name.value;
+        this.refs.name.value = '';
+        if (typeof name === 'string' && name.length > 0) {
+
+            this.setState({
+                name: name
+            });
+        }
+
+    },
     render: function () {
-        var name = this.props.name;
+        var name = this.state.name;
+        var msg = this.props.msg;
         return (
             <div>
                 <h1>Hello {name}!</h1>
-                <p>This is from the component!</p>
+                <p>{msg}</p>
+
+                <form onSubmit={this.onButtonClick}>
+                    <input type="text" ref="name"/>
+                    <button>Set Name</button>
+                </form>
             </div>
         )
     }
 });
 
+var firstName = 'Frank';
+
 ReactDOM.render(
-    <Greeter name="Frank"/>,
+    <Greeter name={firstName} msg="yous uck lol"/>,
     document.getElementById('app')
 );
