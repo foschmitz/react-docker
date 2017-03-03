97569ca806d40e40b99f35720c55de6b7d490db7
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-03-03 14:15:31 +0100

    Added Api Call for OpenWeatherMap

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+const APP_ID = '014b104c9850472d8da0fed17f7a40fd';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/find?units=metric';
+
+module.exports = {
+    getTemp: function (location) {
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}&appid=${APP_ID}&q=${encodedLocation}`;
+
+        console.log(requestUrl);
+
+        return axios.get(requestUrl).then(function (res) {
+            if (res.data.count < 1) {
+                throw new Error("City not found");
+            } else {
+                return res.data.list[0].main.temp;
+            }
+        }, function (res) {
+            throw new Error(res.data.message);
+        });
+
+    }
+}
