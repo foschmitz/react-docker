e1dc1ef4be93b9453f31da62dc2aaf8bf46796a7
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-03-06 13:27:44 +0100

    Nav for CountDownApp

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var Nav = require('Nav');
+
 
 var Main = (props) => {
     return (
         <div>
             <div className="row">
-                <p>Main.jsx rendered</p>
+                <Nav/>
                 <div className="columns medium-6 large-4 small-centered">
                     {props.children}
                 </div>
