2b1d58225d6f7bef167ae5ae7df635860f006724
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-03-06 10:42:06 +0100

    Boilerplate

--- app/components/WeatherMessage.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var WeatherMessage = ({temp, location}) => {
-    return (
-        <h3 className="text-center">It is {temp}&#8451; in {location}</h3>
-    );
-}
-
-module.exports = WeatherMessage;
