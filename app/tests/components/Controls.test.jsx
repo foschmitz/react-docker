b4fc22c87d7a6f3b9bd29b16b55534c0c718cac2
Frank Schmitz <frank.schmitz@rewe-digital.com>
2017-03-07 17:23:27 +0100

    Timer

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -3,30 +3,29 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+
 var Controls = require('Controls');
 
 describe('Controls', () => {
-    it('should exist', () => {
-        expect(Controls).toExist();
+  it('should exist', () => {
+    expect(Controls).toExist();
+  });
+
+  describe('render', () => {
+    it('should render pause when started', () => {
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Pause)');
+
+      expect($pauseButton.length).toBe(1);
     });
 
-    describe('render', () => {
-        it('should render pause when started', () => {
-            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>)
-            var $el = $(ReactDOM.findDOMNode(controls));
+    it('should render start when paused', () => {
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
+      var $el = $(ReactDOM.findDOMNode(controls));
+      var $pauseButton = $el.find('button:contains(Start)');
 
-            var $pauseButton = $el.find('button:contains(Pause)')
-
-            expect($pauseButton.length).toBe(1);
-        });
-        it('should render start when pause', () => {
-            var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>)
-            var $el = $(ReactDOM.findDOMNode(controls));
-
-            var $pauseButton = $el.find('button:contains(Start)')
-
-            expect($pauseButton.length).toBe(1);
-        });
+      expect($pauseButton.length).toBe(1);
     });
-
+  });
 });
